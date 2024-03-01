6d280fb56a41521757a3754793fdd0c4876d61c4
tapsaca <98756983+tapsaca@users.noreply.github.com>
2024-03-01 17:55:57 +0200

    Create the form for signing in

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,58 @@
+import { useFormik } from 'formik'
+import { Pressable, StyleSheet, TextInput, View } from 'react-native'
 import Text from './Text'
+import theme from '../theme'
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: theme.colors.primary,
+    borderRadius: 3,
+    margin: 10,
+    padding: 10
+  },
+  container: {
+    backgroundColor: 'white'
+  },
+  input: {
+    borderRadius: 3,
+    borderWidth: 1,
+    margin: 10,
+    padding: 10
+  }
+})
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    onSubmit: values => {
+      console.log(values)
+    }
+  })
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder='Username'
+        value={formik.values.username}
+        onChangeText={formik.handleChange('username')}
+      />
+      <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder='Password'
+        value={formik.values.password}
+        onChangeText={formik.handleChange('password')}
+      />
+      <Pressable style={styles.button} onPress={formik.handleSubmit}>
+        <Text>Sign In</Text>
+      </Pressable>
+    </View>
+  )
 }
 
 export default SignIn
