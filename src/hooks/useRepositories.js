1892d876a77f3ac4c421db68fe843001b589a658
tapsaca <98756983+tapsaca@users.noreply.github.com>
2024-03-06 08:21:43 +0200

    Replace the Fetch API implementation with a GraphQL query

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,24 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client'
+import { GET_REPOSITORIES } from '../graphql/queries'
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState()
-  const [loading, setLoading] = useState(false)
-
-  const fetchRepositories = async () => {
-    setLoading(true)
-
-    const response = await fetch('http://192.168.1.204:5000/api/repositories')
-    const json = await response.json()
-
-    setLoading(false)
-    setRepositories(json)
-  }
-
-  useEffect(() => {
-    fetchRepositories()
-  }, [])
-
-  return { repositories, loading, refetch: fetchRepositories }
+  const { data } = useQuery(GET_REPOSITORIES, { fetchPolicy: 'cache-and-network' })
+  return { repositories: data?.repositories }
 }
 
 export default useRepositories
